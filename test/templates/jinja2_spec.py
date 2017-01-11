6118d4e3e7867bc930389eb5f933c35c2ffc85af
Pavlos Kleanthous <pavlos.kleanthous@e2x.co.uk>
2017-01-11 12:05:00 +0000

    Add some print messages to make tests more meaningful.

--- test/templates/jinja2_spec.py
+++ test/templates/jinja2_spec.py
@@ -11,6 +11,12 @@ def diff(templates_path, template_file, confg_file, expected_config):
 
     start_timestamp = time.time()
 
+    tmp_text = "Running jinja2Spec...\n"
+    print(colorama.Fore.GREEN + colorama.Style.NORMAL +  tmp_text)
+
+    tmp_text = "Template: %s Configuration file: %s Expected Configuration: %s\n" % (template_file, confg_file, expected_config)
+    print(colorama.Fore.GREEN + colorama.Style.NORMAL +  tmp_text)
+
     colorama.init()
 
     ENV = jinja2.Environment(loader=jinja2.FileSystemLoader(
@@ -45,23 +51,23 @@ def diff(templates_path, template_file, confg_file, expected_config):
             lineNum += 1
         if opcode == "+ ":
             tmp_text = "In Line: %d" % lineNum
-            print(colorama.Fore.GREEN + tmp_text)
+            print(colorama.Fore.GREEN + colorama.Style.NORMAL +  tmp_text)
             tmp_text = "Expected: %s" % expected_line
-            print(colorama.Fore.GREEN + tmp_text)
+            print(colorama.Fore.GREEN + colorama.Style.NORMAL + tmp_text)
             tmp_text = "Got:      %s" % (line[2:].strip())
-            print(colorama.Fore.RED + tmp_text)
+            print(colorama.Fore.RED + colorama.Style.NORMAL + tmp_text)
             error_count += 1
 
     if error_count == 0:
         tmp_text = "You are awesome! jinja2 spec passed successfully"
-        print(colorama.Fore.YELLOW + tmp_text)
+        print(colorama.Fore.YELLOW + colorama.Style.NORMAL + tmp_text)
     else:
         tmp_text = "jinja2 spec found %d errors" % error_count
         print(colorama.Fore.RED + colorama.Style.BRIGHT + tmp_text)
 
     elapsed_time = time.time() - start_timestamp
-    tmp_text = "Elapsed Time: %.3f Seconds" % elapsed_time
-    print(colorama.Fore.GREEN + tmp_text)
+    tmp_text = "Elapsed Time: %.3f Seconds\n" % elapsed_time
+    print(colorama.Fore.GREEN + colorama.Style.NORMAL + tmp_text)
 
     if os.path.exists(".jinja2_spec"):
         shutil.rmtree(".jinja2_spec")
--- test/templates/test.py
+++ test/templates/test.py
@@ -1,9 +1,14 @@
-import jinja2_spec 
+import jinja2_spec
 
 jinja2_spec.diff(templates_path='../../napalm_junos/templates',
                     template_file='set_hostname.j2',
                     confg_file='maroulla.yaml',
                     expected_config='set_hostname.expected')
+
+jinja2_spec.diff(templates_path='../../napalm_junos/templates',
+                    template_file='set_hostname.j2',
+                    confg_file='maroulla.yaml',
+                    expected_config='set_hostname_maroulla.expected')
 
 jinja2_spec.diff(templates_path='../../napalm_junos/templates',
                     template_file='set_hostname.j2',
